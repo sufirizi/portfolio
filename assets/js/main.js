88c42becbc55beb00331cdf6b9d1c69f6118e122
Abu Sufyan <abu.sufyan@codeclouds.co.in>
2023-09-05 16:44:06 +0530

    Footer added

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,8 +21,11 @@ console.log('Automatic git push addedd again!!!!');
 
 $('.owl-carousel').owlCarousel({
     loop:true,
+    autoplay: true,
     margin:10,
+    dots: true,
     nav:true,
+    navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
     responsive:{
         0:{
             items:1
